b0c60ce41ef31234611a866424abc835e5e528c2
= <ryan.catoen@gmail.com>
2016-06-28 21:23:24 -0400

    Added constants for protocols

--- rpc.h
+++ rpc.h
@@ -17,6 +17,15 @@ extern "C" {
 #define ARG_INPUT   31
 #define ARG_OUTPUT  30
 
+// For Protocols
+#define REGISTER        1
+#define LOC_REQUEST     2
+#define LOC_SUCCESS     3
+#define LOC_FAILURE     4
+#define EXECUTE         5
+#define EXECUTE_SUCCESS 6
+#define EXECUTE_FAILURE 7
+#define TERMINATE       8
 
 typedef int (*skeleton)(int *, void **);
 
