e961e4666d606f7c346628aaa1b69dd69e581658
bnm-92 <babarnaveedmemon@gmail.com>
2016-07-03 20:09:06 -0400

    babar - added rpc init for a server to connect to the binder using env variables

--- rpc.cpp
+++ rpc.cpp
@@ -9,9 +9,59 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <signal.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+void *get_in_addr(struct sockaddr *sa);
 
 int rpcInit(){
-    return 0;
+    // this will create a socket for a server with a random 
+    //port and connect to the binder
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    char* hostname = getenv("BINDER_ADDRESS");
+    char* port  = getenv("BINDER_PORT");
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(hostname, (char*)port, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "server: failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
+    printf("server: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo);
+    return sockfd;
 }
 
 int connectToSocket(int port, hostent* server){
@@ -173,5 +223,16 @@ int rpcExecute(){
 
 int rpcTerminate(){
     return 0;
+}
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
