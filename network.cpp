9955da1622899c3d5a48ecf95aa968005d3a1abc
= <ryan.catoen@gmail.com>
2016-07-08 16:03:59 -0400

    Remove &

--- network.cpp
+++ network.cpp
@@ -147,7 +147,7 @@ void send_args(int sockid, int* argTypes, void** args){
     for (int i=0; i<len_at-1; i++){
         int type = get_arg_type(argTypes[i]);
         int arg_len = get_arg_length(argTypes[i]);
-        send(sockid, &args[i], numBytes(type, arg_len), 0);
+        send(sockid, args[i], numBytes(type, arg_len), 0);
         printf("sent arg\n");
     }
 }
