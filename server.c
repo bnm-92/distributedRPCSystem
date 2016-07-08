2350447b94d14a84740c86cf91ce3bbeadce8e3a
bnm-92 <babarnaveedmemon@gmail.com>
2016-07-08 19:39:54 -0400

    test client server for only f0

--- server.c
+++ server.c
@@ -27,41 +27,41 @@ int main(int argc, char *argv[]) {
   argTypes0[2] = (1 << ARG_INPUT) | (ARG_INT << 16);
   argTypes0[3] = 0;
 
-  argTypes1[0] = (1 << ARG_OUTPUT) | (ARG_LONG << 16);
-  argTypes1[1] = (1 << ARG_INPUT) | (ARG_CHAR << 16);
-  argTypes1[2] = (1 << ARG_INPUT) | (ARG_SHORT << 16);
-  argTypes1[3] = (1 << ARG_INPUT) | (ARG_INT << 16);
-  argTypes1[4] = (1 << ARG_INPUT) | (ARG_LONG << 16);
-  argTypes1[5] = 0;
+  // argTypes1[0] = (1 << ARG_OUTPUT) | (ARG_LONG << 16);
+  // argTypes1[1] = (1 << ARG_INPUT) | (ARG_CHAR << 16);
+  // argTypes1[2] = (1 << ARG_INPUT) | (ARG_SHORT << 16);
+  // argTypes1[3] = (1 << ARG_INPUT) | (ARG_INT << 16);
+  // argTypes1[4] = (1 << ARG_INPUT) | (ARG_LONG << 16);
+  // argTypes1[5] = 0;
 
-  /* 
-   * the length in argTypes2[0] doesn't have to be 100,
-   * the server doesn't know the actual length of this argument
-   */
-  argTypes2[0] = (1 << ARG_OUTPUT) | (ARG_CHAR << 16) | 100;
-  argTypes2[1] = (1 << ARG_INPUT) | (ARG_FLOAT << 16);
-  argTypes2[2] = (1 << ARG_INPUT) | (ARG_DOUBLE << 16);
-  argTypes2[3] = 0;
+  // /* 
+  //  * the length in argTypes2[0] doesn't have to be 100,
+  //  * the server doesn't know the actual length of this argument
+  //  */
+  // argTypes2[0] = (1 << ARG_OUTPUT) | (ARG_CHAR << 16) | 100;
+  // argTypes2[1] = (1 << ARG_INPUT) | (ARG_FLOAT << 16);
+  // argTypes2[2] = (1 << ARG_INPUT) | (ARG_DOUBLE << 16);
+  // argTypes2[3] = 0;
 
-  /*
-   * f3 takes an array of long. 
-  */
-  argTypes3[0] = (1 << ARG_OUTPUT) | (1 << ARG_INPUT) | (ARG_LONG << 16) | 11;
-  argTypes3[1] = 0;
+  // /*
+  //  * f3 takes an array of long. 
+  // */
+  // argTypes3[0] = (1 << ARG_OUTPUT) | (1 << ARG_INPUT) | (ARG_LONG << 16) | 11;
+  // argTypes3[1] = 0;
 
-  /* same here, 28 is the exact length of the parameter */
-  argTypes4[0] = (1 << ARG_INPUT) | (ARG_CHAR << 16) | 28;
-  argTypes4[1] = 0;
+  // /* same here, 28 is the exact length of the parameter */
+  // argTypes4[0] = (1 << ARG_INPUT) | (ARG_CHAR << 16) | 28;
+  // argTypes4[1] = 0;
 
   /* 
    * register server functions f0~f4
    */
   printf("registering\n");
   rpcRegister("f0", argTypes0, *f0_Skel);
-  rpcRegister("f1", argTypes1, *f1_Skel);
-  rpcRegister("f2", argTypes2, *f2_Skel);
-  rpcRegister("f3", argTypes3, *f3_Skel);
-  rpcRegister("f4", argTypes4, *f4_Skel);
+  // rpcRegister("f1", argTypes1, *f1_Skel);
+  // rpcRegister("f2", argTypes2, *f2_Skel);
+  // rpcRegister("f3", argTypes3, *f3_Skel);
+  // rpcRegister("f4", argTypes4, *f4_Skel);
 
   /* call rpcExecute */
   printf("executing\n");
