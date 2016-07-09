bc12ff1e33f9d2c0ce8fddfedae1614942dee8b4
bnm-92 <babarnaveedmemon@gmail.com>
2016-07-09 10:50:48 -0400

    changed rpc.h to original, moved everything to network.h

--- network.h
+++ network.h
@@ -1,3 +1,25 @@
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+ 
+// For Protocols
+#define REGISTER        	1
+#define REGISTER_SUCCESS	2
+#define REGISTER_FAILURE	3
+#define LOC_REQUEST     	4
+#define LOC_SUCCESS     	5
+#define LOC_FAILURE     	6
+#define EXECUTE         	7
+#define EXECUTE_SUCCESS 	8
+#define EXECUTE_FAILURE 	9
+#define TERMINATE       	10
+
+void *get_in_addr(struct sockaddr *sa);
+int getPort(struct sockaddr *sa);
+
 void send_integer(int sockid, int val);
 
 void send_string(int sockid, char* val);
